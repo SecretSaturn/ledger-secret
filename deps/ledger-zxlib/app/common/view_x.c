1ab54026709d651149beafeff5fc266e0e96d962
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2021-11-22 22:14:17 +0100

    Changed credits

--- deps/ledger-zxlib/app/common/view_x.c
+++ deps/ledger-zxlib/app/common/view_x.c
@@ -61,8 +61,10 @@ UX_STEP_CB(ux_idle_flow_4_step, bn, h_secret_click(), { "Developed by:", "Zondax
 UX_STEP_NOCB(ux_idle_flow_4_step, bn, { "Developed by:", "Zondax.ch", });
 #endif
 
-UX_STEP_NOCB(ux_idle_flow_5_step, bn, { "License:", "Apache 2.0", });
-UX_STEP_CB(ux_idle_flow_6_step, pb, os_sched_exit(-1), { &C_icon_dashboard, "Quit",});
+UX_STEP_NOCB(ux_idle_flow_5_step, bn, { "Modified by:", "Secret Saturn", });
+
+UX_STEP_NOCB(ux_idle_flow_6_step, bn, { "License:", "Apache 2.0", });
+UX_STEP_CB(ux_idle_flow_7_step, pb, os_sched_exit(-1), { &C_icon_dashboard, "Quit",});
 
 const ux_flow_step_t *const ux_idle_flow [] = {
   &ux_idle_flow_1_step,
@@ -71,6 +73,7 @@ const ux_flow_step_t *const ux_idle_flow [] = {
   &ux_idle_flow_4_step,
   &ux_idle_flow_5_step,
   &ux_idle_flow_6_step,
+  &ux_idle_flow_7_step,
   FLOW_END_STEP,
 };
 
